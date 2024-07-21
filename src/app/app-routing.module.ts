7554c9b089bd6dd4c512207f2bc23264fd76cf7a
Diana Regina <devdianaregina@gmail.com>
2024-07-21 20:47:57 -0300

    :sparkles: criando os componentes iniciais

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,14 @@ import { APP_BASE_HREF } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { EmptyRouteComponent } from './empty-route/empty-route.component';
+import { HomeComponent } from './pages/home/home.component';
 
-const routes: Routes = [{ path: '**', component: EmptyRouteComponent }];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
